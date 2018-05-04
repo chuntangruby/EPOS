fbb3a9c2a36e7662f222f84b6f1f4959fb77b902
tangchun <tangchun@anji-tec.com>
2018-05-04 10:52:25 +0800

    增加店家的页面

---
+++ EPOS_DataBase/Dataresult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPOS_DataBase
+{
+    public class DataResult
+    {
+        //返回数据
+        public object DataValue { get; set; }
+        //执行结果
+        public bool ExeResult { get; set; }
+    }
+}
